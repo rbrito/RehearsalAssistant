15b69bfa40e3e1a9a12185f26dc589483c1014df
srajko <srajko@5b55c6c6-8c8e-4e46-ab4c-115d7207317c>
2009-09-15 02:40:22 +0000

    widget internationalization branch
    git-svn-id: https://svn.code.sf.net/p/rehearsalassist/code@79 5b55c6c6-8c8e-4e46-ab4c-115d7207317c

---
+++ android_sound_recorder_widget/branches/internationalization/src/org/urbanstew/RehearsalAssistant/RecordWidget/RecordWidgetActivity.java
@@ -0,0 +1,16 @@
+package org.urbanstew.RehearsalAssistant.RecordWidget;
+
+import org.urbanstew.RehearsalAssistant.RecordWidget.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecordWidgetActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity);
+    }
+}
